45341933f526410aaa42818540bf7d4198cb60cf
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-05-25 17:50:13 +0200

    2/3: harmadik futó felvétele

--- src/main/java/hu/gde/runnersdemo/DataLoader.java
+++ src/main/java/hu/gde/runnersdemo/DataLoader.java
@@ -55,6 +55,27 @@ public class DataLoader implements CommandLineRunner {
         runnerEntity2.getLaptimes().add(laptime4);
 
         runnerRepository.save(runnerEntity2);
+
+        //Harmadik futó felvétele
+        RunnerEntity runnerEntity3 = new RunnerEntity();
+        runnerEntity3.setRunnerName("Pál");
+        runnerEntity3.setAveragePace(150);
+        runnerEntity3.setHeight(170);
+
+        LapTimeEntity laptime5 = new LapTimeEntity();
+        laptime5.setLapNumber(1);
+        laptime5.setTimeSeconds(95);
+        laptime5.setRunner(runnerEntity3);
+
+        LapTimeEntity laptime6 = new LapTimeEntity();
+        laptime6.setLapNumber(2);
+        laptime6.setTimeSeconds(100);
+        laptime6.setRunner(runnerEntity3);
+
+        runnerEntity3.getLaptimes().add(laptime5);
+        runnerEntity3.getLaptimes().add(laptime6);
+
+        runnerRepository.save(runnerEntity3);
     }
 }
 
