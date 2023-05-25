0363efe0b3cdc297b8cb0c91743418aba8512778
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-05-25 18:56:32 +0200

    2/10: rest végpont sponsor megváltoztatásához

--- src/main/java/hu/gde/runnersdemo/RunnerRestController.java
+++ src/main/java/hu/gde/runnersdemo/RunnerRestController.java
@@ -14,11 +14,13 @@ public class RunnerRestController {
     @Autowired
     private LapTimeRepository lapTimeRepository;
     private RunnerRepository runnerRepository;
+    private SponsorRepository sponsorRepository;
 
     @Autowired
-    public RunnerRestController(RunnerRepository runnerRepository, LapTimeRepository lapTimeRepository) {
+    public RunnerRestController(RunnerRepository runnerRepository, LapTimeRepository lapTimeRepository, SponsorRepository sponsorRepository) {
         this.runnerRepository = runnerRepository;
         this.lapTimeRepository = lapTimeRepository;
+        this.sponsorRepository = sponsorRepository;
     }
 
     @GetMapping("/{id}")
@@ -101,6 +103,21 @@ public class RunnerRestController {
         }
     }
 
+    @PostMapping("/{id}/setsponsor")
+    public ResponseEntity setSponsor(@PathVariable Long id,
+                                     @RequestBody SponsorRequest sponsorRequest) {
+        RunnerEntity runner = runnerRepository.findById(id).orElse(null);
+        SponsorEntity sponsor =
+                sponsorRepository.findById(sponsorRequest.getSponsorId()).orElse(null);
+        if(runner != null && sponsor != null) {
+            runner.setSponsor(sponsor);
+            runnerRepository.save(runner);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Runner with ID " + id + " not found");
+        }
+    }
+
     public static class LapTimeRequest {
         private int lapTimeSeconds;
 
@@ -110,6 +127,18 @@ public class RunnerRestController {
 
         public void setLapTimeSeconds(int lapTimeSeconds) {
             this.lapTimeSeconds = lapTimeSeconds;
+        }
+    }
+
+    public static class SponsorRequest {
+        private long sponsorId;
+
+        public long getSponsorId() {
+            return sponsorId;
+        }
+
+        public void setSponsorId(long sponsorId) {
+            this.sponsorId = sponsorId;
         }
     }
 }
